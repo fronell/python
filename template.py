e716d4fc7c4a14cc7074633b4cc81f9be421e352
Chris Wornell <fro.888@gmail.com>
2017-04-20 01:50:43 -0500

    template.py: Support for passing specific log level, .gitignore: Ignore Vim .swp files

--- template.py
+++ template.py
@@ -14,6 +14,7 @@ from optparse import OptionParser
 EXE_VERSION = '0.0.1'
 # basename is used to strip the ./ if the script is executed from the local dir
 EXE_NAME    = os.path.basename(sys.argv[0])
+LOG_LEVELS  = ('debug', 'info')
 
 
 ################################### GLOBALS ###################################
@@ -27,14 +28,14 @@ pp = pprint.PrettyPrinter(indent = 2)
 def init_log(log_level):
     logger = logging.getLogger('logger')
 
-    if log_level == 'DEBUG':
+    if log_level == 'debug':
         logger.setLevel(logging.DEBUG)
         # log_format = '%(asctime)s.%(msecs)d | %(module)s | %(funcName)s:%(lineno)d | %(levelname)-5s | %(message)s'
-        # The -5s for levelname makes it so the INFO and DEBUG columns line up
+        # The -5s for levelname makes it so the INFO,DEBUG,ERROR columns line up
         log_format  = '%(asctime)s.%(msecs)d | %(funcName)s:%(lineno)d | %(levelname)-5s | %(message)s'
     else:
         logger.setLevel(logging.INFO)
-        log_format  = '%(asctime)s.%(msecs)d | %(levelname)s | %(message)s'
+        log_format  = '%(asctime)s.%(msecs)d | %(levelname)-5s | %(message)s'
     
     date_format = '%Y-%m-%d %I:%M:%S'
     formatter   = logging.Formatter(log_format, date_format)
@@ -47,22 +48,19 @@ def init_log(log_level):
 
 
 ############################### OPTION PARSING ################################
-opts_parser = OptionParser(version="%prog " + EXE_VERSION)
-opts_parser.add_option('-d', '--debug', action='store_true', 
-                       help='enable debug logging')
+opts_parser = OptionParser(version="%prog {}".format(EXE_VERSION))
+opts_parser.add_option('-l', '--log_level', choices=LOG_LEVELS,
+                       default='debug', help='choices are {}'.format(LOG_LEVELS))
 (opts, args) = opts_parser.parse_args()
 
 num_args_required = 0
 num_args_passed   = len(args)
 
 if num_args_passed != num_args_required:
-    opts_parser.error("Args passed=%d, Args required=%d" % (num_args_passed, 
-                                                            num_args_required))
+    opts_parser.error("Args passed={}, Args required={}".format(num_args_passed, 
+                                                                num_args_required))
     
-if opts.debug:
-    log = init_log('DEBUG')
-else:
-    log = init_log('INFO')
+log = init_log(opts.log_level)
 
 
 #################################### MAIN #####################################
@@ -71,6 +69,8 @@ startTime = datetime.datetime.now()
 log.info("[=============== BEGIN ===============]")
 log.info("%s %s starting...", EXE_NAME, EXE_VERSION)
 log.debug("Arguments passed: %s", sys.argv[1:])
+log.warn("This is a warning")
+log.error("This is an error")
 
 duration = datetime.datetime.now() - startTime
 log.info("status=Complete duration_seconds=%d", duration.seconds)
